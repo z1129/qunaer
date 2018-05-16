13eeda4f755077191b451337e80986ff5c337e2e
z1129 <17853586132@163.com>
2018-05-16 21:26:15 +0800

    swiper完成

--- src/main.js
+++ src/main.js
@@ -11,10 +11,19 @@ import "styles/reset.css"
 import "styles/border.css"
 //  引入iconfont的css
 import iconfont from "styles/iconfont.css"
+//  引入swiper插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+//  引入swiper需要的css文件
+import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 // 移动端300 毫秒点击延迟的问题
 fastClick.attach(document.body)
+
+// use引入的组件
+Vue.use(VueAwesomeSwiper, /* { default global options } */);
+
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for = "item of swiperList" :key = "item.id">
+                <img class = "swiper-img" :src="item.imgUrl">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"HomeSwiper" ,
+    data (){
+        return {
+            swiperOption:{
+                pagination:".swiper-pagination",
+                loop:true
+            },
+            swiperList:[
+                {id:1,imgUrl:"https://imgs.qunarzz.com/vc/18/9b/1b/2fea915c71f5981976721720ec.jpg_92.jpg"},
+                {id:2,imgUrl:"https://imgs.qunarzz.com/vc/a3/61/71/7e6365a9a73427604e7e4eecee.jpg_92.jpg"}
+            ]
+        }
+       
+    }   
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active 
+        background:#fff !important
+    .wrapper
+        width:100%
+        height:0
+        overflow :hidden
+        padding-bottom:51.73%
+        .swiper-img
+            width:100%
+</style>
